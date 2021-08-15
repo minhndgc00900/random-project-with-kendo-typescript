194d6285e4864e9b787847c6594091535ce611e7
Minh Dinh Nguyen <minhnd0496@gmail.com>
2021-08-15 23:44:33 +0700

    initial try

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import { useState } from 'react';
+// import { process } from '@progress/kendo-data-query';
+import { Grid, GridColumn } from '@progress/kendo-react-grid';
+import products from './products.json';
+import '@progress/kendo-theme-default/dist/all.css';
 import './App.css';
 
 // tslint:disable-next-line:typedef
 function App() {
-  const [dadas, setdadas] = useState('asdasd');
   return (
     <div className="App">
       <h1>Hello KendoReact!</h1>
+      <Grid
+        data={products}>
+        <GridColumn field="ProductName" />
+        <GridColumn field="UnitPrice" />
+        <GridColumn field="UnitsInStock" />
+        <GridColumn field="Discontinued" />
+      </Grid>
     </div>
   );
 }
